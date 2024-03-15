2b792fd915cf5481125ca415b9a30c67eff2686c
mohanpratap <prtapmohan579@gmail.com>
2024-03-15 14:07:31 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className='nav_bar'></div>
+      <div className='logo'>WDM</div>
+      <div className='search'></div>
+      <div className='seller'></div>
+    </div>
+  )
+}
+
+export default Navbar
